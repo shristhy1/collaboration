10cf65acd77e1e572ca6de0cf63f48889045aea3
pfieffer <cbpmgr@gmail.com>
2017-06-18 17:21:05 +0545

    COL-223[x] Add printPrice()

--- Product.java
+++ Product.java
@@ -7,11 +7,16 @@ public class Product {
 		this.title = title;
 		this.price = price;
 	}
+        
+        public static void printPrice(Product p){
+            System.out.println("The price of " + p.title + " is " + p.price);
+        }
 
 	public static void main (String [] args) {
 		Product laptop = new Product("Mac Pro", 2799.00);
-
-		System.out.println("The price of " + laptop.title + " is " + laptop.price);
+                printPrice(laptop);
+                
+              
 	}
 
     public int getTitle() {
